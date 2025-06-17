42c885449baf05c470bedab714a9aa04ad61ac11
Chatbordin <klinscea@gmail.com>
2025-06-17 14:43:04 +0700

    initial

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
-from typing import Union
+from fastapi import Depends, FastAPI
 
-from fastapi import FastAPI
+from app.dependencies import get_query_token, get_token_header
+from app.internal import admin
+from app.routers import items, users
 
-from llm import display_device
+app = FastAPI(dependencies=[Depends(get_query_token)])
 
-app = FastAPI()
+
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
 
 
 @app.get("/")
-def read_root():
-    display_device()
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+async def root():
+    return {"message": "Hello Bigger Applications!"}
